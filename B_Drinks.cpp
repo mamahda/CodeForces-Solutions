62a3b20e206bcdb022bd9ea13491d62405149a94
Gilbran <gbrnmhd89@gmail.com>
2024-11-20 23:41:59 +0700

    add

---
+++ B_Drinks.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, sum;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int temp;
+        cin >> temp;
+        sum += temp;
+    }
+    double ans = (double)sum / n;
+    cout << fixed << setprecision(12) << ans;
+
+    return 0;
+}
